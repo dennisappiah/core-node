480581c83b356267b7eb43280344e7794f363379
dennisappiah <appiahdennis1998@gmail.com>
2024-07-12 03:45:55 +0000

    updated

---
+++ networking/tcpserver/clients.js
@@ -0,0 +1,10 @@
+const net = require("net");
+
+const socket = net.createConnection({ host: "127.0.0.1", port: 3099 }, () => {
+  // writing buffer to the socket stream
+  const buff = Buffer.alloc(2);
+  buff[0] = 12;
+  buff[1] = 34;
+
+  socket.write(buff);
+});
