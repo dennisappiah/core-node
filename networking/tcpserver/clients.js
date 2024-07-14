0cd808fa3deaecba6a31a3cc8e38347066042705
Dennis Appiah <66980358+dennisappiah@users.noreply.github.com>
2024-07-14 01:57:19 +0000

    Update clients.js

--- networking/tcpserver/clients.js
+++ networking/tcpserver/clients.js
@@ -6,5 +6,6 @@ const socket = net.createConnection({ host: "127.0.0.1", port: 3099 }, () => {
   buff[0] = 12;
   buff[1] = 34;
 
+  // client sending buffer data to the server
   socket.write(buff);
 });
